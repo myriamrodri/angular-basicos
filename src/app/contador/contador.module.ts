2b18da6b4fdeda195ec97cade7497c3a4ea195ee
Myriam <miriam.rodriguez@berger-levrault.com>
2022-10-10 15:55:04 +0200

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { ContadorComponent } from './contador/contador.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [
+        ContadorComponent        
+    ],
+    exports: [
+        ContadorComponent  
+    ],
+    imports: [
+        CommonModule           
+    ]
+
+})
+
+export class contadorModule{
+
+}
